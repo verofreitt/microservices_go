5eaa8780efec8c3f877885bd6f87680427eff5f3
verofreitt <verofreit@gmail.com>
2025-06-30 15:51:47 -0300

    Updates gRPC client to use secure credentials
    Replaces deprecated grpc.WithInsecure() with the new recommended approach using insecure.NewCredentials(). This change follows gRPC best practices for handling insecure connections explicitly.

--- account/client.go
+++ account/client.go
@@ -5,6 +5,7 @@ import (
 	"microservices_go/account/microservices_go/account/pb"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 type Client struct {
@@ -13,7 +14,7 @@ type Client struct {
 }
 
 func NewClient(url string) (*Client, error) {
-	conn, err := grpc.Dial(url, grpc.WithInsecure())
+	conn, err := grpc.NewClient(url, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		return nil, err
 	}
